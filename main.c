44062977854d12b177ed496e79d2ea38079f83c1
DancinParrot <kaizhetan@yahoo.com>
2024-03-10 16:07:48 +0800

    feat: first-fit algorithm to reuse previous unused memory blocks

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <unistd.h> // for sbrk
 
+// Article reference:
+// http://dmitrysoshnikov.com/compilers/writing-a-memory-allocator/#mutator-allocator-collector
 // 64-bit systems would be 8-byte alignment
 typedef unsigned long word_t;
 
@@ -26,6 +28,10 @@ static Block *start = NULL;
 
 static Block *top = NULL;
 
+/*
+ * Align given block to the word boundary (i.e. at least 8 on 64-bit systems or
+ * multiples of 8)
+ */
 size_t align(size_t block_size) {
   return (block_size + sizeof(word_t) - 1) & ~(sizeof(word_t) - 1);
 }
@@ -56,6 +62,29 @@ Block *getBrk(size_t size) {
 }
 
 /*
+ * First-fit approach whereby traverse the linked-list of blocks from start
+ * until an usused block with the same size is found. Otherwise, return null
+ *
+ * Cons: Not fast as need traverse the whole linked-list of unused and used
+ * blocks of different sizes.
+ */
+Block *firstFit(size_t size) {
+  Block *block = start;
+
+  while (block != NULL) {
+    if (block->used || block->size != size) {
+      block = block->next;
+      continue;
+    }
+    return block;
+  }
+
+  return NULL;
+}
+
+Block *findBlock(size_t size) { return firstFit(size); }
+
+/*
  * Move memory by shifting program break (brk) pointer using sbkr()
  * Only invoke this function when previously allocated and unused
  * blocks have been exhausted.
@@ -65,7 +94,13 @@ word_t *alloc(size_t size) {
   // Byte alignment
   size = align(size);
 
-  Block *block = getBrk(size);
+  Block *block = findBlock(size);
+
+  if (block) {
+    return block->data;
+  }
+
+  block = getBrk(size);
 
   block->size = size;
   block->used = true;
@@ -101,15 +136,19 @@ void freeBlock(word_t *data) {
 int main() {
   word_t *data1 = alloc(3);
   Block *block1 = getBlock(data1);
-
   assert(block1->size == sizeof(word_t));
 
-  word_t *data2 = alloc(10);
+  word_t *data2 = alloc(8);
   Block *block2 = getBlock(data2);
-  assert(block2->size == 16);
+  assert(block2->size == 8);
 
+  // Free block2 for later use
   freeBlock(data2);
   assert(block2->used == false);
+
+  word_t *data3 = alloc(8);
+  Block *block3 = getBlock(data3);
+  assert(block3 == block2); // reused previous block2
 
   return 0;
 }
